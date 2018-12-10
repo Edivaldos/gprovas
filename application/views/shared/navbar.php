6073bc29793ae6eaa21f5fb99fe116430b027b87
Edivaldos <31553112+Edivaldos@users.noreply.github.com>
2018-12-09 22:16:19 -0200

    Update navbar.php

--- application/views/shared/navbar.php
+++ application/views/shared/navbar.php
@@ -2,7 +2,7 @@
     <div class="container">
         <a class="navbar-brand" href="#">
             <img src="<?=base_url()?>images/logo-ifg-vertical.png" width="25" height="30" class="d-inline-flex align-top" alt="">
-            IFG Luzi‚nia
+            IFG Luzi√¢nia
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,10 +14,10 @@
                         <span class="sr-only">(current)</span>
                     </a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a class="nav-link" href="<?=base_url()?>sobre">Sobre</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a class="nav-link" href="<?=base_url()?>manuais">Manual do Sistema</a>
                 </li>
             </ul>
